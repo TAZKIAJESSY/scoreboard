f1f3c22222a556c41fd370438277a942ba8aecdf
tazkiajessy <jessy566eee@gmail.com>
2021-02-27 11:54:38 +0100

    add required to form input

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -3,22 +3,22 @@ import React, { useState } from "react";
 function AppPlayerForm(props) {
   const [name, set_name] = useState("");
 
-  function submit(event) {
+  function addHere(event) {
     event.preventDefault();
 
     props.addPlayer(name);
-    //set_name("");
+    set_name("");
 
-    if (!name) {
-      window.alert("Hey fill in the entire form!");
-    } else {
-      props.addPlayer(name);
-      set_name("");
-    }
+    // if (!name) {
+    //   window.alert("Hey fill in the entire form!");
+    // } else {
+    //   props.addPlayer(name);
+    //   set_name("");
+    // }
   }
   return (
     <div className="AddPlayerForm">
-      <form>
+      <form onSubmit={addHere}>
         <label>New player Name:</label>
         <input
           onChange={(event) => {
@@ -28,8 +28,9 @@ function AppPlayerForm(props) {
           type="text"
           placeholder="Name"
           value={name}
+          required
         />{" "}
-        <button onClick={submit}>Add</button>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
